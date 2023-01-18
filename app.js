462aa3200f3b20b8b779bbe63f96be10c1da33e6
Jeremy Ongko <ongkojeremy@gmail.com>
2023-01-17 21:13:52 -0500

    added basic app.js and ejs package

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const pgp = require("pg-promise")();
+const db = pgp("postgres://postgres:password@localhost:5432/full_stack_development");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static("public"));
+
+const { Sequelize, DataTypes } = require("sequelize");
+const { Posts, User } = require("./models");
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
